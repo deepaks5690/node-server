7a7bd1dd3fc1d757c6fdc9385b5454a49a0aff67
Deepak Sagar <deepak.sagar@dotsquares.com>
2024-12-31 15:20:49 +0530

    sending all files on repo

--- openAI.js
+++ openAI.js
@@ -1,7 +1,7 @@
 const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
-    apiKey: "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", // Replace with your API key
+    apiKey: "XXX", // Replace with your API key
 });
 
 

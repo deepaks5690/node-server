704a0c7cfb147b628ce700aa4c49f4ffdedd9b0d
Deepak Sagar <deepak.sagar@dotsquares.com>
2024-12-31 16:17:31 +0530

    Rebuild cache

--- src/routes/emailRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getNesletterEmails,deleteNewsletterEmail } = require('../controllers/newsletterController');
-const auth = require('../middlewares/auth');
-
-router.get('/subscriptions/allpages', auth, getNesletterEmails);
-router.post('/subscriptions/delete_page',auth,deleteNewsletterEmail);
-
-module.exports = router;

b794a032eead253500088ae1fc8ca88b4032a50f
Deepak Sagar <deepak.sagar@dotsquares.com>
2024-12-31 15:39:25 +0530

    sending all files on repo

--- openAI.js
+++ openAI.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 const { OpenAI } = require("openai");
 
-const openai = new OpenAI({
-    apiKey: process.env.OPEN_AI_KEY, // Replace with your API key
-});
+const openai = new OpenAI();
 
 
 
